cadd3294e32f0256d3558471d03648e21fa7cf32
manotik12 <manotik254@gmail.com>
2022-11-13 22:37:24 +0300

    all yale files

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDsebaQbwKI0aoy0ZhBSQD94bxLPVyANjA",
+    authDomain: "hciyale.firebaseapp.com",
+    databaseURL: "https://hciyale-default-rtdb.firebaseio.com",
+    projectId: "hciyale",
+    storageBucket: "hciyale.appspot.com",
+    messagingSenderId: "684490971692",
+    appId: "1:684490971692:web:4e50ba0fdf263ce47c8f89",
+    measurementId: "G-9CBMCCCS67"
+  };
+
+  
